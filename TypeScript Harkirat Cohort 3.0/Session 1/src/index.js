23347a63ed913bbcb30f11a99038bdcf65813ab7
Sambhav <sambhav12wakhariya@gmail.com>
2025-03-05 23:54:09 +0530

    Checked In : TypeScript Harkirat Cohort 3 Session 1

---
+++ TypeScript Harkirat Cohort 3.0/Session 1/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function greet(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+greet("John"); // Output: Hello, John!
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 7)); // Output: 12
---
+++ TypeScript Harkirat Cohort 3.0/Session 1/src/index.ts
@@ -0,0 +1,12 @@
+
+function greet(firstName : string){
+    console.log(`Hello, ${firstName}!`);
+}
+
+greet("John"); // Output: Hello, John!
+
+function sum(a : number , b : number){
+    return a + b;
+}
+
+console.log(sum(5, 7)); // Output: 12
